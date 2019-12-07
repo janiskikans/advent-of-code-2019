24faff6a2e335b42528b2015ffc8a4504ee3c4b0
Janis Kikans <janis.kikans@gmail.com>
2019-12-07 14:53:50 +0200

    day 1, part B

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,8 +1,22 @@
 import math
 
-# Caluclate the required fuel for a module
+# Calculate the required fuel for a module
 def calculate_required_fuel_for_module(mass):
     return math.floor(mass / 3) - 2
+
+# Caluclate the total required fuel for module (including extra additional fuel for fuel)
+def calculate_required_fuel_plus_additional_fuel(mass):
+    total_required_fuel = 0
+    required_fuel = calculate_required_fuel_for_module(mass)
+    
+    while (1):
+        total_required_fuel += required_fuel
+        required_fuel = calculate_required_fuel_for_module(required_fuel)
+        
+        if (required_fuel <= 0):
+            break
+        
+    return(total_required_fuel)
 
 # Read the input file of module masses to a list
 def read_modules_to_array(input_file):
@@ -17,7 +31,7 @@ def get_total_required_fuel():
     
     total_required_fuel = 0
     for module_mass in module_masses:
-        total_required_fuel += calculate_required_fuel_for_module(int(module_mass))
+        total_required_fuel += calculate_required_fuel_plus_additional_fuel(int(module_mass))
         
     return total_required_fuel
     
